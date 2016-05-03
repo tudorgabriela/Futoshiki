5f81ad5097f9815456b633089f4ada2ba334c17e
tudorgabriela <tudorgabriela@yahoo.com>
2016-05-03 23:08:20 +0300

    Update miniproj.py

--- miniproj.py
+++ miniproj.py
@@ -1,15 +1,14 @@
 '''
-
 @author: Tudor Gabriela, Ilie Iulia
 '''
-from PyQt4 import QtGui,QtCore
+import QtGui,QtCore
 import sys
 import random
 
 # definim clasa programului si ceea ce contine aceasta ( fereastra principala, matricea jocului, grila jocului, numarul de patrate)
 
 class Futoshiki_interface(QtGui.QApplication):
-    main_window=""
+    main_window=""  
     game_matrix=""
     game_grid=""
     nrsquares=""
@@ -22,15 +21,9 @@ class Futoshiki_interface(QtGui.QApplication):
         QtGui.QApplication.__init__(self, args) 
         self.initUi()  // Delegăm crearea interfetei utilizatorului la metoda initUI().
     
-    
-    
-   #  
-   
-   
-   
-   
     def initUi(self):
-        
+         #  definim metoda initUI(self) 
+         #  definim functiile ferestrei principale, bara de meniu si optiunea de joc nou
         
         self.main_window=QtGui.QFrame()
         self.main_window.setLayout(QtGui.QVBoxLayout())
@@ -52,17 +45,14 @@ class Futoshiki_interface(QtGui.QApplication):
 
 
 
-    # afisam regulile actiunii   
-
-
+    # afisam regulile actiunii, meniul de ajutor, optiunea hint, imagini si ecranul principal
         showrules_action=QtGui.QAction('&Rules',self)
         showrules_action.triggered.connect(self.showrules)
-        help_menu.addAction(showrules_action)
+        help_menu.addAction(showrules_action) 
         
         showtips_action=QtGui.QAction('Tips',self)
         showtips_action.triggered.connect(self.showtips)
         help_menu.addAction(showtips_action)
-        
         
         imagelabel=QtGui.QLabel(self.main_window)
         imagelabel.setPixmap(QtGui.QPixmap('fimage.jpg'))
@@ -70,17 +60,12 @@ class Futoshiki_interface(QtGui.QApplication):
         
         self.main_window.layout().addWidget(imagelabel)
         
-        
         self.main_window.layout().setMenuBar(menubar)
         self.main_window.show()
 
-
-
-
-    # definim procesul de selectare  a nivelului jocului
+    # definim procesul de selectare  a nivelului jocului(aplicam for)
 
     def selectLevel(self):
-
         for i in reversed(range(self.main_window.layout().count())):
             try: 
                 self.main_window.layout().itemAt(i).widget().setParent(None)
@@ -100,9 +85,7 @@ class Futoshiki_interface(QtGui.QApplication):
         self.main_window.layout().addWidget(hard_button)
         
         
-        
-        
-            
+
     def levelclick(self):
         sender=self.sender()
         if(sender.text()=='Easy'):
@@ -112,7 +95,8 @@ class Futoshiki_interface(QtGui.QApplication):
         else:
             self.newgameclick(22)
     def newgameclick(self,level):
-        #clear everything already present in the layout
+
+        #stergem orice este deja prezent in schema
         for i in reversed(range(self.main_window.layout().count())):
             try: 
                 self.main_window.layout().itemAt(i).widget().setParent(None)
@@ -132,14 +116,10 @@ class Futoshiki_interface(QtGui.QApplication):
         
       
     def verifygrid(self):
-        #verify grid method.
+        #verificam metoda grid.
         
-        
-
         numbermat=[[0 for x in range(5)] for y in range(5)]
         aidlist=[0 for z in range(25)]
-        
-        
         aidlist1=[0 for z in range(25)]
        
         row=0
@@ -172,7 +152,6 @@ class Futoshiki_interface(QtGui.QApplication):
             except:
                 ok=True
         
-        
         verifymatrix=[[0 for x in range(5)] for y in range(5)]
         k=0
         for i in range(0,5):
@@ -180,17 +159,13 @@ class Futoshiki_interface(QtGui.QApplication):
                 verifymatrix[i][j]=aidlist1[k]
                 k=k+1
         
-        
         result=QtGui.QMessageBox(self.main_window)
         if verifymatrix==numbermat:
             
-            
             result.setText('You have completed this puzzle!Great job!')
             
-           
         else:
             result.setText('The solution you have found is not correct.Try again!')
-            
             
         for i in range(0,9,2):
            for j in range(0,9,2):
@@ -201,27 +176,122 @@ class Futoshiki_interface(QtGui.QApplication):
                if self.game_matrix[i][j]!=self.game_grid.itemAtPosition(i,j).widget().toPlainText().strip(' '):
                    x=self.game_grid.itemAtPosition(i, j).widget().toPlainText().strip(' ')
                    
-                   
                    self.game_grid.itemAtPosition(i,j).widget().setStyleSheet('color:red')
-        
-      
-        
         
         
         result.setFixedSize(10,10)
         
-        
-        
         result.setWindowTitle('Result')
         result.show()
              
-
-        
     def showrules(self):
         rulesdialog=QtGui.QDialog(self.main_window)
         rules=QtGui.QTextBrowser(rulesdialog)
-        rules.setText("""What are the rules of Futoshiki?
+        rules.setText("""Care sunt regulile jocului Futoshiki?
 
-
-    
+    Futoshiki este un joc puzzle amuzant. Regulile jocului Futoshiki sunt urmatoarele:
+Incepi cu o grila de forma 5x5, sau 7 x 7 pentru puzzle-uri Futoshiki mai mari..
+Scopul este de a umple fiecare rând și coloană cu numere de la 1 – 5, fiecare aparand o singura dată (presupunând o grilă 5x5) - această regulă este familiară din sudoku.
+Cu toate acestea, nu există regiuni caseta ca în sudoku. Marea diferență sunt  semnele de inegalitate, iar jucatorul  trebuie să se supună acestora în răspunsurile  pe care le va da. De exemplu, dacă avem  o celulă mai mare decât cea de lângă ea, atunci știm ca celula din stânga nu poate fi  1, iar celula din dreapta nu poate fi 5.
+Utilizati  logica pentru a deduce unde merge fiecare  număr pentru a rezolva puzzle-ul.
+""")
+        rules.show()
+        rules.setFixedSize(500,200)
+        rulesdialog.show()
+        rulesdialog.setWindowTitle("Futoshiki rules")
         
+    def showtips(self): 
+        tipsdialog=QtGui.QDialog(self.main_window) 
+        tips=QtGui.QTextBrowser(tipsdialog)
+        tips.setText("""Rezolvarea puzzle-ului necesită o combinație de tehnici logice.  Numerele în fiecare rând și coloană limiteaza numărul de valori posibile pentru fiecare poziție, așa cum fac inegalitățile.
+Odată ce masa de posibilități a fost determinată, o tactică crucială pentru a rezolva puzzle-ului implică "eliminarea AB", în care diverse  subseturi sunt identificate într-un rând al cărui interval de valori pot fi determinate. De exemplu, în cazul în care primele două pătrate într-un rând trebuie să conțină 1 sau 2, atunci aceste numere pot fi excluse din pătratele rămase. În mod similar, în cazul în care primele trei pătrate trebuie să conțină 1 sau 2; 1 sau 3; și 1 sau 2 sau 3, atunci cei rămași trebuie să conțină alte valori (4 și 5 într-un puzzle 5x5).
+O altă tehnică importantă este de a lucra prin gama de posibilități în inegalități deschise. O valoare de pe o parte a unei inegalități determină o alta, care apoi poate fi prelucrata prin intermediul puzzle-ului până când se ajunge la o contradicție, iar prima valoare este exclusă.
+In plus, multe puzzle-uri Futoshiki sunt promise să posede soluții unice
+""")
+        tips.show()
+        tips.setFixedSize(500,200)
+        tipsdialog.show()
+        tipsdialog.setWindowTitle("Tips & Tricks")
+        
+  
+# Putem face de fapt ceva în cazul în care acest scenariu este rulat independent, astfel încât să putem testa 
+# aplicatia noasra, dar, de asemenea, suntem capabili de a importa acest program fără a fi rulat vreun cod.
+def readfile(filename):       
+        matric=[[0 for j in range(9)] for i in range(9)]
+        with open(filename, mode="r",encoding="utf-8") as my_file:
+            animals = my_file.read().strip('\n').split(" ")
+            
+        k=0  
+        for i in range(0,9):
+            for j in range(0,9):
+                matric[i][j]=animals[k].rstrip('\n')
+                k=k+1
+        return matric
+def generategrid(matrice,nrsquares,parent):
+    
+    matrice=readfile("futoshiki1.txt")
+    xi=0
+    
+    while(xi<nrsquares):
+        
+        x=random.randrange(0,10,2)
+        y=random.randrange(0,10,2)
+        if matrice[x][y]!="0":
+                matrice[x][y]="0"
+                xi=xi+1
+                      
+    grid=QtGui.QGridLayout()
+        
+    for i in range (0,9):
+        for j in range (0,9):
+            try:
+                 
+                int(matrice[i][j])
+                    
+                box=QtGui.QTextEdit(matrice[i][j])
+                box.setAlignment(QtCore.Qt.AlignCenter)
+                if int(matrice[i][j])==0:
+                    box.setText("")
+                    
+                    box.setFixedSize(40,40)
+   
+                else:
+                    box.setReadOnly(True)
+                    
+                        
+                    box.setFixedSize(40,40)
+                box.setFont(QtGui.QFont("Courier",16))    
+                grid.addWidget(box,i,j)
+                    
+            except ValueError:
+                if matrice[i][j]=='|' or matrice [i][j]=='-':
+                    label=QtGui.QTextEdit(parent)
+                   
+                    label.setReadOnly(True)
+                    label.setFixedSize(40,40)
+                    label.setStyleSheet('background-color:rgba(139,0,0,0);border:0px')
+                        
+                else:
+                        
+                    label=QtGui.QTextEdit(parent)
+                    label.setText(matrice[i][j])
+                   
+                    label.setAlignment(QtCore.Qt.AlignCenter)
+                    label.setReadOnly(True)
+                    label.setFixedSize(40,40)
+                    label.setStyleSheet('background-color:rgba(139,0,0,0);border:0px;font-size:20px')
+                
+                grid.addWidget(label,i,j)
+                
+    return grid
+
+def printGrid(grid):
+    for i in range(0,5):
+        for j in range(0,5):
+            if i==5:
+                print(grid[i][j])
+            else:
+                print(grid[i][j],end="")
+if __name__ == "__main__":
+    Game = Futoshiki_interface(sys.argv)
+    sys.exit(Game.exec_())
